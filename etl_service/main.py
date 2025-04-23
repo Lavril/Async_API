477d41e2291e3b3afa9338edbb2ae3fd863931a5
mrgervant <vlad.popov98@yandex.ru>
2025-04-23 20:56:14 +0300

    Update core of application for work with DB connectors

--- etl_service/main.py
+++ etl_service/main.py
@@ -1,5 +1,39 @@
 #!/usr/bin/env python
+import time
+
+import elasticsearch
+import psycopg
+
+from core.backoff import backoff
+from core.logger import logger
+from core.settings import settings
+
+from db.elastic import ElasticConnector
+from db.postgres import PostgresConnector
+
+
+
+@backoff(psycopg.OperationalError, max_time=100)
+def check_pg(conn_):
+    return conn_.execute('SELECT COUNT(*) FROM content.film_work').fetchall()
+
+
+@backoff(elasticsearch.ConnectionError, max_time=100)
+def check_es(conn_):
+    return conn_.indices.exists(index='movies')
+
+
+def load_from_postgres_to_elastic():
+    postgres = PostgresConnector(settings.postgres_settings.get_dsn())
+    elastic = ElasticConnector(settings.elastic_settings.get_host())
+    with postgres.connect() as pg_conn, elastic.connect() as es_conn:
+        pg = check_pg(pg_conn)
+        logger.info(pg)
+        es = check_es(es_conn)
+        logger.info(es)
 
 
 if __name__ == '__main__':
-    print('Start OK')
+    logger.info('Start ETL application')
+    load_from_postgres_to_elastic()
+    logger.info('Stop ETL application')
