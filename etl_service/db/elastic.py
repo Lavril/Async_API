d87bc72270c7f2376ea790c44884b41ab67c5116
mrgervant <vlad.popov98@yandex.ru>
2025-04-25 06:35:14 +0300

    Update DB connectors

--- etl_service/db/elastic.py
+++ etl_service/db/elastic.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 import logging
+from typing import Any, Generator
 
 import elasticsearch
 from elasticsearch import Elasticsearch
@@ -8,43 +9,32 @@ from core.backoff import backoff
 
 
 class ElasticConnector:
-    """Подключение к Elasticsearch."""
+    """Elasticsearch context manager connector."""
 
     def __init__(self, dsn: str):
         self.dsn = dsn
         self.logger = logging.getLogger(__name__)
 
-    @backoff(elasticsearch.ConnectionError, max_time=100)
-    def _create_client(self):
-        """Метод для подключения с backoff."""
+    @backoff(elasticsearch.ConnectionError)
+    def _create_client(self) -> Elasticsearch:
+        """Create Elasticsearch client with backoff."""
         client = Elasticsearch(self.dsn)
         if not client.ping():
-            raise elasticsearch.ConnectionError("Elasticsearch не доступен")
+            raise elasticsearch.ConnectionError("Elasticsearch don't response")
         return client
 
     @contextmanager
-    def connect(self):
-        """Контекстный менеджер для подключения к Elasticsearch."""
+    def connect(self) -> Generator[Elasticsearch, Any, None]:
+        """Elasticsearch context manager."""
         client = None
         try:
             client = self._create_client()
-            self.logger.info("Подключение с Elasticsearch установлено")
+            self.logger.info('Successful connection to Elasticsearch')
             yield client
         except elasticsearch.ConnectionError:
-            self.logger.exception("Ошибка в Elasticsearch")
+            self.logger.exception('Get connection error from Elasticsearch')
             raise
         finally:
             if client:
                 client.close()
-                self.logger.info("Соединение с Elasticsearch закрыто")
-
-    def create_index_if_not_exists(self, index: str, index_body: dict):
-        """Создать индекс если он не существует."""
-        with self.connect() as client:
-            try:
-                if not client.indices.exists(index=index):
-                    client.indices.create(index=index, body=index_body)
-                    self.logger.info(f"Created index {index}")
-            except Exception:
-                self.logger.exception("Failed to create index")
-                raise
+                self.logger.info('Close connection with Elasticsearch')
