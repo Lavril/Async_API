66a27ec21f285b1d22c41cb0cbbb47e4656be82d
mrgervant <vlad.popov98@yandex.ru>
2025-04-25 20:22:30 +0300

    Hotfix config

--- fast_api/core/config.py
+++ fast_api/core/config.py
@@ -14,7 +14,7 @@ REDIS_HOST = os.getenv('REDIS_HOST', '127.0.0.1')
 REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
 
 # Настройки Elasticsearch
-ELASTIC_SCHEMA = os.getenv('ELASTIC_SCHEMA', 'https://')
+ELASTIC_SCHEMA = os.getenv('ELASTIC_SCHEMA', 'http://')
 ELASTIC_HOST = os.getenv('ELASTIC_HOST', '127.0.0.1')
 ELASTIC_PORT = int(os.getenv('ELASTIC_PORT', 9200))
 
