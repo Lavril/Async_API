6dba69f97079e7d0a0063f2e98f017b4eba51cd4
v_a_sudakov <v_a_sudakov@inbox.ru>
2025-04-27 00:36:01 +0300

    add all response models to fast_api/api/v1/models.py

--- fast_api/api/v1/films.py
+++ fast_api/api/v1/films.py
@@ -1,76 +1,29 @@
 from http import HTTPStatus
-from typing import Optional, List
 
-from fastapi import APIRouter, Depends, HTTPException
-from pydantic import BaseModel
+from fastapi import APIRouter, Depends, HTTPException, Path
 
 from services.film import FilmService, get_film_service
+from .models import FilmFullResponse
 
 # Объект router, в котором регистрируем обработчики
 router = APIRouter()
 
-# FastAPI в качестве моделей использует библиотеку pydantic
-# https://pydantic-docs.helpmanual.io
-# У неё есть встроенные механизмы валидации, сериализации и десериализации
-# Также она основана на дата-классах
 
-
-class Person(BaseModel):
-    uuid: str
-    full_name: str
-
-
-class Genre(BaseModel):
-    uuid: str
-    name: str
-    description: Optional[str] = None
-
-
-# Модель ответа API
-class Film(BaseModel):
-    uuid: str
-    title: str
-    imdb_rating: Optional[float] = None
-    pg_rating: Optional[float] = None
-    description: Optional[str] = None
-    genres: Optional[List[Genre]] = None
-    actors: Optional[List[Person]] = None
-    writers: Optional[List[Person]] = None
-    directors: Optional[List[Person]] = None
-
-
-# С помощью декоратора регистрируем обработчик film_details
-# На обработку запросов по адресу <some_prefix>/some_id
-# Позже подключим роутер к корневому роутеру
-# И адрес запроса будет выглядеть так — /api/v1/film/some_id
-# В сигнатуре функции указываем тип данных, получаемый из адреса запроса (film_id: str)
-# И указываем тип возвращаемого объекта — Film
 @router.get(
     '/{film_id}',
-    response_model=Film,
-    summary="Поиск кинопроизведений по UUID",
-    description="Поиск кинопроизведений по UUID",
-    response_description="Название и рейтинг фильма",)
-async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
+    response_model=FilmFullResponse,
+    summary="Получить информацию о кинопроизведении",
+    description="Возвращает полную информацию о кинопроизведении по его идентификатору",
+    response_description="Объект с информацией о кинопроизведении",)
+async def film_details(
+        film_id: str = Path(..., example="3d825f60-9fff-4dfe-b294-1a45fa1e115d", description="UUID фильма"),
+        film_service: FilmService = Depends(get_film_service)
+) -> FilmFullResponse:
     film = await film_service.get_by_id(film_id)
-    print(f"Film object: {film}")  # Для отладки
-    print(f"Film type: {type(film)}")  # Для отладки
-    print(f"Film type: {film.uuid}")  # Для отладки
-    print(f"Film type: {type(film.uuid)}")  # Для отладки
-    print(f"Film type: {film.title}")  # Для отладки
-    print(f"Film type: {type(film.title)}")  # Для отладки
     if not film:
-        # Если фильм не найден, отдаём 404 статус
-        # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum    # Такой код будет более поддерживаемым
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
 
-    # Перекладываем данные из models.Film в Film
-    # Обратите внимание, что у модели бизнес-логики есть поле description,
-    # которое отсутствует в модели ответа API.
-    # Если бы использовалась общая модель для бизнес-логики и формирования ответов API,
-    # вы бы предоставляли клиентам данные, которые им не нужны
-    # и, возможно, данные, которые опасно возвращать
-    return Film(
+    return FilmFullResponse(
         uuid=film.uuid,
         title=film.title,
         imdb_rating=film.imdb_rating,
---
+++ fast_api/api/v1/models.py
@@ -0,0 +1,140 @@
+from datetime import datetime
+from typing import Optional, List
+
+from pydantic import Field, BaseModel, HttpUrl
+
+
+class Person(BaseModel):
+    """Модель персоны (актёра, режиссёра, сценариста)"""
+    uuid: str = Field(
+        ...,
+        example="5b4bf1bc-3397-4e83-9b17-8b10c6544ed1",
+        description="Уникальный идентификатор персоны"
+    )
+    full_name: str = Field(
+        ...,
+        example="George Lucas",
+        description="Полное имя персоны",
+        max_length=255
+    )
+
+
+class Genre(BaseModel):
+    """Модель жанра фильма"""
+    uuid: str = Field(
+        ...,
+        example="3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff",
+        description="Уникальный идентификатор жанра"
+    )
+    name: str = Field(
+        ...,
+        example="Action",
+        description="Название жанра",
+        max_length=50
+    )
+    description: Optional[str] = Field(
+        None,
+        example="Фильмы с динамично развивающимся сюжетом",
+        description="Описание жанра",
+        max_length=1000
+    )
+
+
+class FilmBaseResponse(BaseModel):
+    """Базовая информация по кинопроизведениям"""
+    uuid: str = Field(
+        ...,
+        example="3d825f60-9fff-4dfe-b294-1a45fa1e115d",
+        description="Уникальный идентификатор фильма"
+    )
+    title: str = Field(
+        ...,
+        example="Star Wars: Episode IV - A New Hope",
+        description="Название кинопроизведения",
+        max_length=255
+    )
+
+
+class FilmShortResponse(FilmBaseResponse):
+    """Короткая информация по кинопроизведениям"""
+    imdb_rating: Optional[float] = Field(
+        None,
+        example=8.6,
+        description="Рейтинг кинопроизведения на IMDB",
+        ge=0,
+        le=10
+    )
+    genres: Optional[List[Genre]] = Field(
+        None,
+        description="Список жанров кинопроизведения"
+    )
+
+
+class FilmFullResponse(FilmShortResponse):
+    """Полная информация по кинопроизведениям"""
+    description: Optional[str] = Field(
+        None,
+        example="The Imperial Forces...",
+        description="Описание кинопроизведения",
+        max_length=2000
+    )
+    pg_rating: Optional[str] = Field(
+        None,
+        description="Возрастной рейтинг (PG, PG-13, R и т.д.)",
+        example="PG-13",
+        max_length=10
+    )
+    duration: Optional[int] = Field(
+        None,
+        description="Длительность фильма в минутах",
+        example=120,
+        gt=0
+    )
+    release_year: Optional[int] = Field(
+        None,
+        description="Год выпуска кинопроизведения",
+        example=1977,
+        ge=1888,  # Первый фильм был снят в 1888 году
+        le=datetime.now().year + 5  # Плюс небольшой запас на будущее
+    )
+    poster_url: Optional[HttpUrl] = Field(
+        None,
+        description="URL постера фильма",
+        example="https://example.com/posters/star_wars.jpg"
+    )
+    actors: Optional[List[Person]] = Field(
+        None,
+        description="Список актёров"
+    )
+    writers: Optional[List[Person]] = Field(
+        None,
+        description="Список сценаристов"
+    )
+    directors: Optional[List[Person]] = Field(
+        None,
+        description="Список режиссёров"
+    )
+
+
+class PersonFilmsRoles(FilmBaseResponse):
+    """Модель ролей в фильме"""
+    roles: Optional[List["str"]] = Field(
+        None,
+        description="Должность человека"
+    )
+
+
+class PersonFullResponse(Person):
+    """Полная информация по персоне"""
+    films: Optional[List[PersonFilmsRoles]] = Field(
+        None,
+        description="Список кинопроизведений с должностями человека"
+    )
+
+
+class GenreFullResponse(Genre):
+    """Полная информация по жарнам"""
+    films: Optional[List[FilmBaseResponse]] = Field(
+        None,
+        description="Список кинопроизведений"
+    )
