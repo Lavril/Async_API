753b4dbce3e4a25e681ab87f598c607bb43f87e5
v_a_sudakov <v_a_sudakov@inbox.ru>
2025-05-11 22:51:37 +0300

    Added fictures for ClientSession, es_data, make_get_request

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,5 +1,7 @@
 import asyncio
+import uuid
 
+import aiohttp
 from elasticsearch import AsyncElasticsearch
 import pytest_asyncio
 from elasticsearch.helpers import async_bulk
@@ -9,6 +11,7 @@ from .settings import test_settings
 
 @pytest_asyncio.fixture(scope='session')
 def event_loop():
+    """Фикстура для переопределения event_loop для использования scope session"""
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     yield loop
@@ -17,15 +20,56 @@ def event_loop():
 
 @pytest_asyncio.fixture(name='es_client', scope='session')
 async def es_client():
+    """Фикстура для единоразового создания клиента Elasticsearch"""
     es_client = AsyncElasticsearch(hosts=test_settings.elastic_settings.get_host(), verify_certs=False)
     yield es_client
     await es_client.close()
 
 
+@pytest_asyncio.fixture()
+async def aiohttp_session():
+    """Фикстура для создания одного экземпляра aiohttp.ClientSession  в рамках функции-теста."""
+    session = aiohttp.ClientSession()
+    yield session
+    await session.close()
+
+
+@pytest_asyncio.fixture(name='es_data', scope='session')
+async def es_data():
+    """Фикстура для подготовки тестовых данных для Elasticsearch"""
+    es_data = [{
+        'uuid': str(uuid.uuid4()),
+        'imdb_rating': 8.5,
+        'title': 'The Star',
+        'description': 'New World',
+        'genres': [{'uuid': str(uuid.uuid4()), 'name': 'Action'}, {'uuid': str(uuid.uuid4()), 'name': 'Sci-Fi'}],
+        'directors': [{'uuid': str(uuid.uuid4()), 'full_name': 'Stan'}],
+        'actors': [
+            {'uuid': 'ef86b8ff-3c82-4d31-ad8e-72b69f4e3f95', 'full_name': 'Ann'},
+            {'uuid': 'fb111f22-121e-44a7-b78f-b19191810fbf', 'full_name': 'Bob'}
+        ],
+        'writers': [
+            {'uuid': 'caf76c67-c0fe-477e-8766-3ab3ff2574b5', 'full_name': 'Ben'},
+            {'uuid': 'b45bd7bc-2e16-46d5-b125-983d356768c6', 'full_name': 'Howard'}
+        ],
+        'directors_names': ['Stan'],
+        'actors_names': ['Ann', 'Bob'],
+        'writers_names': ['Ben', 'Howard']
+    } for _ in range(60)]
+
+    bulk_query: list[dict] = []
+    for row in es_data:
+        data = {'_index': 'movies', '_id': row['uuid']}
+        data.update({'_source': row})
+        bulk_query.append(data)
+
+    return bulk_query
+
+
 @pytest_asyncio.fixture(name='es_write_data')
 async def es_write_data(es_client):
     """Фикстура для записи тестовых данных в Elasticsearch"""
-    async def inner(data: list[dict], MAPPING_MOVIES=None):
+    async def inner(data, MAPPING_MOVIES=None):
         if await es_client.indices.exists(index=test_settings.es_index):
             await es_client.indices.delete(index=test_settings.es_index)
         await es_client.indices.create(index=test_settings.es_index, **MAPPING_MOVIES)
@@ -35,4 +79,16 @@ async def es_write_data(es_client):
 
         if errors:
             raise Exception('Ошибка записи данных в Elasticsearch')
+    return inner
+
+
+@pytest_asyncio.fixture(name='make_get_request')
+async def make_get_request(aiohttp_session):
+    """Фикстура для выполнения GET-запросов к API"""
+    async def inner(field: str, endpoint: str, query_data):
+        url = test_settings.fastapi_settings.get_host() + '/api/v1' + field + endpoint + "/"
+        async with aiohttp_session.get(url, params=query_data) as response:
+            body = await response.json()
+            status = response.status
+        return {"body": body, "status": status}
     return inner
--- tests/functional/src/test_search.py
+++ tests/functional/src/test_search.py
@@ -1,6 +1,3 @@
-import uuid
-
-import aiohttp
 import pytest
 
 from ..settings import test_settings
@@ -23,48 +20,8 @@ from ..settings import test_settings
     ]
 )
 @pytest.mark.asyncio
-async def test_search(es_write_data, query_data, expected_answer):
-    # 1. Генерируем данные для ES
-    es_data = [{
-        'uuid': str(uuid.uuid4()),
-        'imdb_rating': 8.5,
-        'title': 'The Star',
-        'description': 'New World',
-        'genres': [{'uuid': str(uuid.uuid4()), 'name': 'Action'}, {'uuid': str(uuid.uuid4()), 'name': 'Sci-Fi'}],
-        'directors': [{'uuid': str(uuid.uuid4()), 'full_name': 'Stan'}],
-        'actors': [
-            {'uuid': 'ef86b8ff-3c82-4d31-ad8e-72b69f4e3f95', 'full_name': 'Ann'},
-            {'uuid': 'fb111f22-121e-44a7-b78f-b19191810fbf', 'full_name': 'Bob'}
-        ],
-        'writers': [
-            {'uuid': 'caf76c67-c0fe-477e-8766-3ab3ff2574b5', 'full_name': 'Ben'},
-            {'uuid': 'b45bd7bc-2e16-46d5-b125-983d356768c6', 'full_name': 'Howard'}
-        ],
-        'directors_names': ['Stan'],
-        'actors_names': ['Ann', 'Bob'],
-        'writers_names': ['Ben', 'Howard']
-    } for _ in range(60)]
-
-    bulk_query: list[dict] = []
-    for row in es_data:
-        data = {'_index': 'movies', '_id': row['uuid']}
-        data.update({'_source': row})
-        bulk_query.append(data)
-
-    # 2. Загружаем данные в ES
-    await es_write_data(bulk_query, test_settings.es_index_mapping)
-
-    # 3. Запрашиваем данные из ES по API
-
-    session = aiohttp.ClientSession()
-    url = test_settings.fastapi_settings.get_host() + '/api/v1/films/search/'
-    async with session.get(url, params=query_data) as response:
-        body = await response.json()
-        # headers = response.headers
-        status = response.status
-    await session.close()
-
-    # 4. Проверяем ответ
-
-    assert status == expected_answer['status']
-    assert len(body) == expected_answer['length']
+async def test_search(make_get_request, es_write_data, es_data: list[dict], query_data: dict, expected_answer: dict):
+    await es_write_data(es_data, test_settings.es_index_mapping)
+    response = await make_get_request('/films', '/search', query_data)
+    assert response['status'] == expected_answer['status']
+    assert len(response['body']) == expected_answer['length']
