520d81a40241f71e7ccf73565a7d8978a9e2334c
mrgervant <vlad.popov98@yandex.ru>
2025-07-09 09:49:57 +0300

    Add tests for /films/search and /persons/search

--- tests/functional/src/api/v1/test_search.py
+++ tests/functional/src/api/v1/test_search.py
@@ -14,14 +14,91 @@ from tests.functional.settings import test_settings
             {'status': 200, 'length': 50}
         ),
         (
+            {'query': 'The Star', 'size': 2},
+            {'status': 200, 'length': 2}
+        ),
+        (
+            {'query': 'The Star', 'size': 62},
+            {'status': 200, 'length': 60}
+        ),
+        (
+            {'query': 'The Star', 'size': 101},
+            {'status': 422, 'length': 1}
+        ),
+        (
             {'query': 'Mashed potato'},
             {'status': 404, 'length': 1}
-        )
+        ),
+        (
+            {'query': ''},
+            {'status': 422, 'length': 1}
+        ),
+        (
+            {},
+            {'status': 422, 'length': 1}
+        ),
+        (
+            {'page': 0},
+            {'status': 422, 'length': 1}
+        ),
+        (
+            {'size': 0},
+            {'status': 422, 'length': 1}
+        ),
     ]
 )
 @pytest.mark.asyncio
-async def test_search(make_get_request, es_write_data, es_data: list[dict], query_data: dict, expected_answer: dict):
-    await es_write_data(es_data, test_settings.es_index_mapping)
+async def test_search_films(make_get_request, es_write_data, es_data_movies: list[dict], query_data: dict, expected_answer: dict):
+    await es_write_data(es_data_movies, 'movies')
     response = await make_get_request('/films', '/search', query_data)
+    assert response['status'] == expected_answer['status']
+    assert len(response['body']) == expected_answer['length']
+
+
+@pytest.mark.parametrize(
+    'query_data, expected_answer',
+    [
+        (
+            {'query': 'Stan'},
+            {'status': 200, 'length': 10}
+        ),
+        (
+            {'query': 'Stan', 'size': 2},
+            {'status': 200, 'length': 2}
+        ),
+        (
+            {'query': 'Stan', 'size': 12},
+            {'status': 200, 'length': 10}
+        ),
+        (
+            {'query': 'Stan', 'size': 101},
+            {'status': 422, 'length': 1}
+        ),
+        (
+            {'query': 'Ron'},
+            {'status': 404, 'length': 1}
+        ),
+        (
+            {'query': ''},
+            {'status': 422, 'length': 1}
+        ),
+        (
+            {},
+            {'status': 422, 'length': 1}
+        ),
+        (
+            {'page': 0},
+            {'status': 422, 'length': 1}
+        ),
+        (
+            {'size': 0},
+            {'status': 422, 'length': 1}
+        ),
+    ]
+)
+@pytest.mark.asyncio
+async def test_search_persons(make_get_request, es_write_data, es_data_persons: list[dict], query_data: dict, expected_answer: dict):
+    await es_write_data(es_data_persons, 'persons')
+    response = await make_get_request('/persons', '/search', query_data)
     assert response['status'] == expected_answer['status']
     assert len(response['body']) == expected_answer['length']
