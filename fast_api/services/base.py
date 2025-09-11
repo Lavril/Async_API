a83fb42459a6dabadf857dfe632092bc2fdb9f5c
mrgervant <vlad.popov98@yandex.ru>
2025-09-11 17:36:44 +0300

    Add abstract class for services cache

--- fast_api/services/base.py
+++ fast_api/services/base.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from collections.abc import Awaitable
+from datetime import timedelta
 
 
 class Service(ABC):
@@ -19,4 +21,19 @@ class SearchMixin(ABC):
     """Миксин для функции поиска сущности."""
     @abstractmethod
     async def search(self, *args, **kwargs):
+        pass
+
+
+class Cache(ABC):
+    """Абстрактный класс для работы с кэшем."""
+    @abstractmethod
+    async def get(self,
+            name: bytes | str | memoryview) -> Awaitable:
+        pass
+
+    @abstractmethod
+    async def set(self,
+            name: bytes | str | memoryview,
+            value: bytes | memoryview | str | int | float,
+            ex: int | timedelta | None = None) -> Awaitable:
         pass
