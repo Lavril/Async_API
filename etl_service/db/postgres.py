0b2cff20b8a73dce67ec0b8b1ad29c0b2471d8ee
mrgervant <vlad.popov98@yandex.ru>
2025-09-16 15:55:23 +0300

    Refactor PostgresConnector to PostgresConnectionFactory

--- etl_service/db/postgres.py
+++ etl_service/db/postgres.py
@@ -4,30 +4,37 @@ from typing import Any, Generator
 
 import psycopg
 from psycopg import connect, Connection, ClientCursor
+from psycopg import OperationalError as PGOperationalError
 from psycopg.conninfo import make_conninfo
 from psycopg.rows import dict_row
 
 from core.backoff import backoff
 
+#вынесем создание коннекта отдельно
+class PostgresConnectionFactory:
+    """Create PostgresSQL connection with backoff."""
+    def __init__(self, dsn: dict):
+        self.dsn = make_conninfo(**dsn)
+
+    @backoff(PGOperationalError)
+    def create(self) -> Connection:
+        return connect(self.dsn, row_factory=dict_row,
+                       cursor_factory=ClientCursor)
+
 
 class PostgresConnector:
     """PostgresSQL context manager connector."""
 
-    def __init__(self, dsn: dict):
-        self.dsn = make_conninfo(**dsn)
+    def __init__(self, postgres_factory: PostgresConnectionFactory):
+        self.postgres_factory = postgres_factory
         self.logger = logging.getLogger(__name__)
-
-    @backoff(psycopg.OperationalError)
-    def _create_connection(self) -> Connection:
-        """Create PostgresSQL connection with backoff."""
-        return connect(self.dsn, row_factory=dict_row, cursor_factory=ClientCursor)
 
     @contextmanager
     def connect(self) -> Generator[Connection, Any, None]:
         """PostgresSQL context manager."""
         conn = None
         try:
-            conn = self._create_connection()
+            conn = self.postgres_factory.create()
             self.logger.info('Successful connection to PostgresSQL')
             yield conn
         except psycopg.OperationalError:
