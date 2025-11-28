5469f41ab3e44f6e7a87730ab497044277826d38
v_a_sudakov <v_a_sudakov@inbox.ru>
2025-11-28 20:43:05 +0300

    Remove os.getenv from Settings

--- auth-service/src/core/config.py
+++ auth-service/src/core/config.py
@@ -1,8 +1,6 @@
-import os
 from logging import config as logging_config
 from pathlib import Path
 
-from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
 from core.logger import LOGGING
@@ -10,25 +8,21 @@ from core.logger import LOGGING
 # Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
 
-# Загружаем .env до создания настроек
-env_path = Path(__file__).parent.parent.parent / '.env'
-load_dotenv(env_path)
-
 
 class Settings(BaseSettings):
     # Переменные прокидываются в окружение контейнера на этапе сборки
     debug: bool = False
     base_dir: Path = Path(__file__).resolve().parent.parent
-    project_name: str = os.getenv('PROJECT_NAME', 'auth')
+    project_name: str = 'auth'
 
-    redis_host: str = os.getenv('REDIS_HOST', '127.0.0.1')
-    redis_port: int = int(os.getenv('REDIS_PORT', 6379))
+    redis_host: str = '127.0.0.1'
+    redis_port: int = 6379
 
-    postgres_user: str = os.getenv('POSTGRES_USER', 'postgres')
-    postgres_password: str = os.getenv('POSTGRES_PASSWORD')
-    postgres_host: str = os.getenv('POSTGRES_HOST', '127.0.0.1')
-    postgres_port: int = int(os.getenv('POSTGRES_PORT', 5432))
-    postgres_db: str = os.getenv('POSTGRES_DB', 'auth')
+    postgres_user: str = 'postgres'
+    postgres_password: str = ""
+    postgres_host: str = '127.0.0.1'
+    postgres_port: int = 5432
+    postgres_db: str = 'auth'
 
 
 settings = Settings()
